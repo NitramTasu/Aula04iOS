efa7193737f854cbfa4bf31dc4443edf116b6a43
Martin Schwans <amrastasu@gmail.com>
2019-02-06 22:17:44 -0200

    Initial Commit

--- Aula04/ViewController.swift
+++ Aula04/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbMessage: UILabel!
+    @IBOutlet weak var lbCount: UILabel!
+    @IBOutlet weak var botoes: UISegmentedControl!
+    var orders = [
+        "Saindo o café",
+        "Preparando um chá",
+        "Pegando uma cachaça"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lbMessage.text = "Testando o texto"
     }
 
-
+    @IBAction func changeLabel(_ sender: Any) {
+        
+        lbMessage.text = "Mudou o texto"
+    }
+    
+    @IBAction func changeOrger(_ sender: UISegmentedControl) {
+        lbMessage.text = orders[sender.selectedSegmentIndex]
+        
+    }
+    @IBAction func printValue(_ sender: UISlider) {
+        lbCount.text = String(Int(sender.value))
+        x
+    }
 }
 
